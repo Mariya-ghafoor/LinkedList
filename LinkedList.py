66d5c83684ebec6f5ce857470d8cedfa07339df5
Mariya Kashif <mariyakashif@Mariyas-MacBook-Air.local>
2023-03-24 23:03:51 +1100

    Add method to find Linked list cycle

--- LinkedList.py
+++ LinkedList.py
@@ -37,10 +37,10 @@ class LinkedList(object):
 
         cur =  self.head
         if pos == -1:
-            return 'no cycle'
+            return None
         #Tasking the cursor to last node
         while cur.next is not None:
-            print('cur.next is ',cur.next.val)
+            # print('cur.next is ',cur.next.val)
             cur = cur.next
         last_node = cur
 
@@ -53,17 +53,40 @@ class LinkedList(object):
 
         #connecting last node with the cycle node at position 'pos'
         last_node.next = cycle_node
+#.................................................
+
+    def find_cycle(self):
+        node = self.head
+        MySet = set()
+        #Iterating through the linked list
+        while node != None:
+            #checking if node already exists in Myset. If yes then we have found the node where last node connects to form cycle.
+            if node in MySet:
+                return node
+            #if node is not in MySet then add it and move on to the next node
+            MySet.add(node)
+            node = node.next
+        return
+
+
 
 
 #Creating a sample liked list [3,2,0,4]
 l1 = LinkedList()
-node = ListNode(3)
+node = ListNode(1)
 l1.head = node
 l1.add_list(2)
-l1.add_list(0)
+l1.add_list(7)
 l1.add_list(-4)
+l1.add_list(19)
+l1.add_list(6)
+l1.add_list(-9)
+l1.add_list(-5)
+l1.add_list(-5)
+l1.add_list(-5)
 l1.print_list()
 
-#Connecting last node of the list with node at pos = 1
-# 3-->2-->0-->-4 link to-->2-->0-->4 .........
-l1.create_cycle(1)
+#Connecting last node of the list with node at pos = 6
+l1.create_cycle(6)
+
+print(l1.find_cycle().val) #printing node value to check if its the right node
